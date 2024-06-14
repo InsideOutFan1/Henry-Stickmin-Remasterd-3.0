7b2b2f4a5974d4077d55b593dbd83beee660483e
InsideOutFan1 <166262263+InsideOutFan1@users.noreply.github.com>
2024-06-14 14:40:28 -0700

    Create RuffleCode.js

---
+++ RuffleCode.js
@@ -0,0 +1,8 @@
+<script>
+    window.RufflePlayer = window.RufflePlayer || {};
+    window.RufflePlayer.config = {
+        "warnOnUnsupportedContent": false, 
+        "autoplay": "on", 
+        "unmuteOverlay": "hidden"
+    };
+</script>
